f9e04f8dea122c0958c85f55a9bfa6bb7499b15a
mcgarad <mcgarad@gmail.com>
2022-02-11 14:17:53 -0500

    Build and finish game function

--- javascript/script.js
+++ javascript/script.js
@@ -4,15 +4,12 @@ function computerPlay() {
     switch (a) {
         case 0:
             computerMove = "Rock";
-            console.log("Rock");
             break;
         case 1:
             computerMove = "Paper";
-            console.log("Paper");
             break;
         case 2:
             computerMove = "Scissors";
-            console.log("Scissors")
             break;
     }
     return computerMove
@@ -20,41 +17,78 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     let resultMessage = "nothing"
+    let result = "none"
     let playerSelectionCased = playerSelection.toLowerCase();
     playerSelectionCased = playerSelectionCased.replace(playerSelectionCased[0], playerSelectionCased[0].toUpperCase());
     if (playerSelectionCased === computerSelection) {
         resultMessage = `It's a tie. ${playerSelectionCased} ties with ${computerSelection}.`;
+        result = "tie"
     } else if (playerSelectionCased === "Rock") {
         switch (computerSelection) {
             case "Paper":
                 resultMessage = `You lose! ${computerSelection} beats ${playerSelectionCased}.`;
+                result = "loss"
                 break;
             case "Scissors":
                 resultMessage = `You win! ${playerSelectionCased} beats ${computerSelection}.`;
+                result = "win"
                 break;
         }
     } else if (playerSelectionCased === "Paper") {
         switch (computerSelection) {
             case "Scissors":
                 resultMessage = `You lose! ${computerSelection} beats ${playerSelectionCased}.`;
+                result = "loss"
                 break;
             case "Rock":
                 resultMessage = `You win! ${playerSelectionCased} beats ${computerSelection}.`;
+                result = "win"
                 break;
         }
     } else if (playerSelectionCased === "Scissors") {
         switch (computerSelection) {
             case "Rock":
                 resultMessage = `You lose! ${computerSelection} beats ${playerSelectionCased}.`;
+                result = "loss"
                 break;
             case "Paper":
                 resultMessage = `You win! ${playerSelectionCased} beats ${computerSelection}.`;
+                result = "win"
                 break;
         }
     } else {resultMessage = "Invalid input given."}
-    return resultMessage;
+    return [result, resultMessage, playerSelectionCased];
 }
 
-const playerSelection = "PApEr"
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    let compScore = 0;
+    let playerScore = 0;
+    alert('Play a game of "Rock, Paper, Scissors". Best of 5 wins.')
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = prompt('Choose "Rock", "Paper", or "Scissors".');
+        let resultList = playRound(playerSelection, computerSelection);
+        if (resultList[0] === "win") {
+            playerScore += 1;
+        } else if (resultList[0] === "loss") {
+            compScore += 1;
+        } else if (resultList[0] === "none") {
+            i -= 1;
+        }
+        console.log(`Computer selection: ${computerSelection}`);
+        console.log(`Player selection: ${resultList[2]}`);
+        console.log(resultList[1]);
+        console.log(`Score: ${playerScore} to ${compScore}`)
+        console.log("")
+    }
+    console.log("")
+    if (compScore == playerScore) {
+        console.log("You tied.");
+    } else if (compScore > playerScore) {
+        console.log("You lose. Better luck next time!");
+    } else if (compScore < playerScore) {
+        console.log("You win!! Have a cookie.")
+    }
+}
+
+
